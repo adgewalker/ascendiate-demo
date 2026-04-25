61f87b5043c0da94cc7f0e6f64e3c3c1fed384f0
Adrian Walker <adge.walker@gmail.com>
2026-04-25 17:44:27 +0100

    feat: changed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://adgewalker.github.io',
-  base: '/github-pages-demo',
   vite: {
     plugins: [tailwindcss()],
   },
